bef171fcce9ee28ac8f0f03a4b42b9fdfef0d426
Serk <serkan.sahin98@outlook.com>
2020-03-25 16:06:31 +0000

    Update R6Api.cs
    Store session in memory over program lifecycle instead of instance lifecycle
    Refactored to extract common request steps for reducing duplications

--- R6Sharp/R6Api.cs
+++ R6Sharp/R6Api.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using R6Sharp.ResponseTypes;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Mime;
 using System.Text;
@@ -15,7 +16,8 @@ namespace R6Sharp
         /// </summary>
         private readonly string _credentialsb64;
 
-        public string Ticket { get; private set; }
+        // Make this static so it persists in memory over current program instance
+        public static Session CurrentSession { get; private set; }
 
         public R6Api(string email, string password)
         {
@@ -34,40 +36,76 @@ namespace R6Sharp
             _credentialsb64 = Convert.ToBase64String(bytes);
         }
 
-        public async Task<string> GetTokenAsync(bool rememberMe = true)
+        public async Task<string> BuildRequestAsync(Uri uri, KeyValuePair<HttpRequestHeader, string>[] AdditionalHeaderValues, byte[] Data, bool Get = true)
         {
-            // get token from Ubisoft
-            var result = "";
-
-            var request = WebRequest.CreateHttp(new Uri(Endpoints.Sessions));
-            request.Method = "POST";
-            request.Headers.Add(HttpRequestHeader.ContentType, MediaTypeNames.Application.Json);    // For rememberMe post
-            request.Headers.Add(HttpRequestHeader.Authorization, $"Basic {_credentialsb64}");       // Authorization header
-            request.Headers.Add("Ubi-AppId", Constants.RainbowUUID);                                // For access
-
-            if (rememberMe)
+            // Build a web request to endpoint
+            var request = WebRequest.CreateHttp(uri);
+            // Set request method
+            request.Method = Get ? "GET" : "POST";
+            // Apply usual request headers that should be in all requests to Ubisoft
+            request.Headers.Add(HttpRequestHeader.ContentType, MediaTypeNames.Application.Json);
+            request.Headers.Add("Ubi-AppId", Constants.RainbowUUID);
+            // Apply auxiliary headers supplied to method
+            foreach (var AdditionalHeaderValue in AdditionalHeaderValues)
             {
-                var data = Encoding.UTF8.GetBytes("{\"rememberMe\": true}");
-                request.ContentLength = data.Length;
+                request.Headers.Add(AdditionalHeaderValue.Key, AdditionalHeaderValue.Value);
+            }
+
+            // If we have some data to send, write it to stream (make sure it is POST)
+            if (Data != null && request.Method.Equals("POST"))
+            {
+                request.ContentLength = Data.Length;
                 using (var stream = request.GetRequestStream())
                 {
-                    await stream.WriteAsync(data).ConfigureAwait(false);
+                    await stream.WriteAsync(Data).ConfigureAwait(false);
                 }
             }
 
+            string result;
+            // Get result from Ubisoft and grab the json
             using (var response = (HttpWebResponse)request.GetResponse())
-            using (var stream = response.GetResponseStream())
             {
-                var bytes = new byte[response.ContentLength];
-                stream.Read(bytes);
-
-                var content = Encoding.UTF8.GetString(bytes);
-                var json = JsonConvert.DeserializeObject<Session>(content);
-                result = rememberMe ? json.RememberMeTicket : json.Ticket;
+                using (var stream = response.GetResponseStream())
+                {
+                    var bytes = new byte[response.ContentLength];
+                    stream.Read(bytes);
+                    result = Encoding.GetEncoding(response.CharacterSet).GetString(bytes);
+                }
             }
 
-            Ticket = result;
             return result;
+        }
+
+        public async Task<string> GetTokenAsync(bool rememberMe = true)
+        {
+            // If a session exist(s/ed), check if it has expired
+            bool expired = true;
+            if (CurrentSession != null)
+            {
+                var now = DateTime.UtcNow;
+                if (now < CurrentSession.Expiration)
+                {
+                    expired = false;
+                }
+            }
+
+            // If any session expired or doesn't exist (by default)
+            if (expired)
+            {
+                // Build json for remembering (or not) the user/session
+                byte[] data = Encoding.UTF8.GetBytes($"{{\"rememberMe\": {rememberMe.ToString().ToLower()}}}");
+                // Add authorization header
+                var headervaluepairs = new KeyValuePair<HttpRequestHeader, string>[]
+                {
+                    new KeyValuePair<HttpRequestHeader, string>(HttpRequestHeader.Authorization, $"Basic {_credentialsb64}")
+                };
+
+                // Get result from endpoint
+                var response = await BuildRequestAsync(new Uri(Endpoints.Sessions), headervaluepairs, data, false);
+                CurrentSession = JsonConvert.DeserializeObject<Session>(response);
+            }
+
+            return rememberMe ? CurrentSession.RememberMeTicket : CurrentSession.Ticket;
         }
     }
 }
