2c49bf91f63ba3a6508a433baa69fbc6865c34cb
SergeantSerk <14278530+sergeantserk@users.noreply.github.com>
2020-09-17 15:51:36 +0100

    Persisting session details over process lifecycle
    Authentication with Ubisoft servers now persists over process lifecycles such that session details are saved locally and expiration is checked before using the ticket.

--- R6Sharp/Endpoint/SessionEndpoint.cs
+++ R6Sharp/Endpoint/SessionEndpoint.cs
@@ -1,7 +1,9 @@
 ﻿using R6Sharp.Response;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -36,8 +38,62 @@ namespace R6Sharp.Endpoint
 
         public async Task<string> GetTicketAsync()
         {
-            // Refresh current session details (will get new session if expired or non-existent)
-            _currentSession = await GetSessionAsync().ConfigureAwait(false);
+            var sessionFileName = "session.json";
+            var requestNewSession = false;
+            // If Session exists in memory, check if it has expired
+            if (_currentSession != null)
+            {
+                var now = DateTime.UtcNow;
+                if (now >= _currentSession.Expiration)
+                {
+                    // Session expired, get new session
+                    requestNewSession = true;
+                }
+            }
+            else
+            {
+                // This is the first time starting session, check if session details were saved as JSON
+                try
+                {
+                    var json = File.ReadAllText(sessionFileName);
+                    var loadedSession = JsonSerializer.Deserialize<Session>(json);
+
+                    // If Session was readable and there is time before expiration
+                    if (loadedSession != null && DateTime.UtcNow < loadedSession.Expiration)
+                    {
+                        // Use the loaded session
+                        _currentSession = loadedSession;
+                    }
+                    else
+                    {
+                        // Get new session
+                        requestNewSession = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    // Session file was not found or it was malformed/invalid
+                    if (e is FileNotFoundException || e is JsonException)
+                    {
+                        requestNewSession = true;
+                    }
+                    else
+                    {
+                        // Error is unknown, rethrow it
+                        throw e;
+                    }
+                }
+            }
+
+            if (requestNewSession)
+            {
+                // Refresh current session details (will get new session if expired or non-existent)
+                _currentSession = await GetSessionAsync().ConfigureAwait(false);
+                // Save new session to file
+                var serialisedSession = JsonSerializer.Serialize<Session>(_currentSession);
+                File.WriteAllText(sessionFileName, serialisedSession);
+            }
+
             return _currentSession.Ticket;
         }
 
